a6f5b8ad6db56ef53fa3ae6e6ee5d06179da8f38
blale-zhang <359593234.gz@gmail.com>
2015-07-19 22:00:40 +0800

    无修改

--- src/org/blade/personal/dao/base/EntityMapper.java
+++ src/org/blade/personal/dao/base/EntityMapper.java
@@ -3,6 +3,7 @@ package org.blade.personal.dao.base;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import javax.persistence.Column;
 
 import org.springframework.jdbc.core.RowMapper;
@@ -28,12 +29,12 @@ public class EntityMapper<T> implements RowMapper<T> {
 		try {
 			t = (T) this.clazz.newInstance();
 			Field[] fields = clazz.getDeclaredFields();
+			
 			for (Field f : fields) {
 				Column a = (Column) f.getAnnotation(Column.class);
 				if(null == a){
 					continue;
 				}
-				f.getType();
 				f.setAccessible(true);
 				f.set(t, rs.getObject(a.name()));
 				f.setAccessible(false);
