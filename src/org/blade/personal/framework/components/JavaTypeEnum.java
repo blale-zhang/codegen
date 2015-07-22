3642d73d6d8e103fdbfb4d09605e5b03b7b50367
blale-zhang <359593234.gz@gmail.com>
2015-07-23 01:08:45 +0800

    修改API，使API更好用

--- src/org/blade/personal/framework/components/JavaTypeEnum.java
+++ src/org/blade/personal/framework/components/JavaTypeEnum.java
@@ -2,7 +2,10 @@ package org.blade.personal.framework.components;
 
 public enum JavaTypeEnum {
 	
-	STRING("java.lang.String"),INTEGER("java.lang.Integer"),FLOAT("java.lang.Float");
+	STRING("java.lang.String"),
+	INTEGER("java.lang.Integer"),
+	FLOAT("java.lang.Float"),
+	DATE("java.utils.Date");
 	
 	String type;
 	
--- src/org/blade/personal/framework/entity/AbstractEntityAlies.java
+++ src/org/blade/personal/framework/entity/AbstractEntityAlies.java
@@ -3,6 +3,7 @@ package org.blade.personal.framework.entity;
 import java.lang.reflect.Field;
 
 import org.blade.personal.framework.components.annotation.Column;
+import org.blade.personal.framework.components.annotation.Table;
 import org.blade.personal.framework.orm.EntityAlies;
 import org.blade.personal.framework.orm.base.FloatAlies;
 import org.blade.personal.framework.orm.base.IntegerAlies;
@@ -14,21 +15,24 @@ import org.blade.personal.framework.orm.base.StringAlies;
  *
  * @param <T>
  */
-public class AbstractEntityAlies<T> implements EntityAlies<T> {
+public abstract class AbstractEntityAlies<T> implements EntityAlies<T> {
 
 	/**
 	 * The alies of  table.
 	 */
 	protected String alies;
+	
+	private String tableName;
 
 	/**
-	 * 构造函数
+	 * 构造函数,用来初始化子类的各属性的对象
 	 * @param alies
 	 */
 	public AbstractEntityAlies(String alies) {
 		this.alies = alies;
 		Field[] fields = getClass().getFields();
-		
+		Table table = getClass().getAnnotation(Table.class);
+		this.tableName = table.name();
 		for( Field field : fields ){
 			Column column = field.getAnnotation(Column.class);
 			try {
@@ -44,6 +48,8 @@ public class AbstractEntityAlies<T> implements EntityAlies<T> {
 					case FLOAT: field.setAccessible(true);
 					field.set(this, new FloatAlies().newInstance(column.name(), 0.0f, this.alies));
 					field.setAccessible(false);
+					break;
+				default:
 					break;
 				}
 			}catch (IllegalArgumentException e) {
@@ -64,6 +70,14 @@ public class AbstractEntityAlies<T> implements EntityAlies<T> {
 		this(null);
 	}
 	
+	@Override
+	public String getEntityName() {
+		return this.getClass().getName();
+	}
 	
+	@Override
+	public String getTable() {
+		return this.tableName;
+	}
 
 }
