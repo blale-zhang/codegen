4e8534e9a8dd41d3ed74a33a5cc3af3ed330f8b3
blale-zhang <359593234.gz@gmail.com>
2015-07-22 21:41:34 +0800

    实体别名类

--- src/org/blade/personal/framework/orm/EntityAlies.java
+++ src/org/blade/personal/framework/orm/EntityAlies.java
@@ -7,10 +7,4 @@ package org.blade.personal.framework.orm;
  */
 public interface EntityAlies<T> {
 
-	/**
-	 * 产生实体别名
-	 * @param instance
-	 * @return
-	 */
-	public EntityAlies<T> create(T instance);
 }
